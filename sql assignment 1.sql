5a9c6ec8a4fa44a652c8288ab66e13ff2c817634
prathmeshpalkar <148679060+prathmeshpalkar@users.noreply.github.com>
2023-12-22 12:02:02 +0530

    Add files via upload

---
+++ sql assignment 1.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE Ages (
+  name VARCHAR(128),
+  age INTEGER
+)
+INSERT INTO Ages (name, age) VALUES ('Mara', 28);
+INSERT INTO Ages (name, age) VALUES ('Otto', 33);
+INSERT INTO Ages (name, age) VALUES ('Fyn', 31);
+INSERT INTO Ages (name, age) VALUES ('Neshawn', 17);
+
+select * from Ages
+SELECT SUBSTRING(CONVERT(VARCHAR(MAX), CONVERT(VARBINARY(MAX), CONCAT(name, age)), 1), 3, LEN(CONVERT(VARCHAR(MAX), CONVERT(VARBINARY(MAX), CONCAT(name, age)), 1))) AS X FROM ages ORDER BY X;
+
